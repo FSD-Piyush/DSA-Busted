dd0a58e3b64d8212ca9aa6d9c26015cd8b6783e4
Piyush Kumar Sahu <113818438+FSD-Piyush@users.noreply.github.com>
2023-11-01 13:15:53 +0530

    Add files via upload

---
+++ Lec 02/2DataType.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   /* int a = 123;
+    cout << a << endl;
+
+    char b = 'v';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.2356;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a is : " << size << endl;
+
+    int dsize = sizeof(d);
+    cout << "Size of d is : " << dsize << endl; */
+
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    unsigned int b = 112;
+    cout << b << endl;
+
+}
