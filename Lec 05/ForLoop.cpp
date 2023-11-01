5727180f7029c43ae0c40abbcf72502a77de7e70
Piyush Kumar Sahu <113818438+FSD-Piyush@users.noreply.github.com>
2023-11-01 13:18:06 +0530

    Add files via upload

---
+++ Lec 05/ForLoop.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    /*int n;
+    cout << " Enter the value of n " << endl;
+    cin >> n;
+    cout << "Printing count from 1 to n" << endl;
+    for(int i=1;i<=n;i++) {
+        cout<<i<<endl;
+    }*/
+
+    /*int n;
+    cin >> n;
+    int sum = 0;
+    for (int i=1; i<=n; i++) {
+        sum += i;
+    }
+    cout << sum << endl;*/
+
+    // Fibonacci Series/Sequence
+    /*int n = 10;
+    int a = 0;
+    int b = 1;
+    cout << a << " " << b << endl;
+    for (int i=1; i<=n; i++) {
+        int c = a+b;
+        cout << c << endl;
+        a = b;
+        b = c;
+    }*/
+
+    /*int n;
+    cin >> n;
+    bool isPrime = 1;
+    for (int i=2; i<n; i++) {
+        if (n%i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    if (isPrime == 0) {
+        cout << " Not a Prime Number " << endl;
+    } else {
+        cout << " Is a Prime Number " << endl;
+    }*/
+
+    
+}
---
+++ Lec 05/HWQues2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    /*for(int i=0;i<=5;i++) {
+        cout << i << " " << endl;
+        i++;
+    }*/
+
+    /*for(int i=0;i<=5;i--) {
+        cout << i << " " << endl;
+        i++;
+    }*/
+
+    /*for(int i=0;i<=15;i+=2) {
+        cout << i << " ";
+        if(i&1) {
+            continue;
+        }
+        i++;
+    }*/
+
+/*int n;
+cin >> n;
+    int rows = n;
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<rows-i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }*/
+    
+
+}
